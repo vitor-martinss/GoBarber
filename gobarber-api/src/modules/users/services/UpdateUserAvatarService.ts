ba0dc0ef269ffddf0557db85638a16636b6866e0
Vitor Martins <vitormartinsweb@gmail.com>
2020-12-26 21:54:28 +0000

    UPD finished all tests - TDD

--- gobarber-api/src/modules/users/services/UpdateUserAvatarService.ts
+++ gobarber-api/src/modules/users/services/UpdateUserAvatarService.ts
@@ -3,6 +3,7 @@ import fs from 'fs'
 import uploadConfig from '@config/upload'
 import { injectable, inject} from 'tsyringe'
 import IUsersRepository from '../repositories/IUsersRepository'
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider'
 import AppError from '@shared/errors/AppError'
 import User from '../infra/typeorm/entities/User'
 
@@ -15,7 +16,10 @@ interface IRequest {
 class UpdateUseravatarService {
 	constructor(
 		@inject('UsersRepository')
-		private usersRepository: IUsersRepository
+		private usersRepository: IUsersRepository,
+
+		@inject('StorageProvider')
+		private storageProvider: IStorageProvider
 	) {}
 
 	public async execute({ user_id, avatarFilename}: IRequest): Promise<User> {
@@ -28,17 +32,11 @@ class UpdateUseravatarService {
 
 		if(user.avatar) {
 			//Deletar avatar anterior
-
-			const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar)
-
-			const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath)
-
-			if(userAvatarFileExists) {
-				await fs.promises.unlink(userAvatarFilePath)
-			}
+			await this.storageProvider.deleteFile(user.avatar)
 		}
 
-		user.avatar = avatarFilename
+		const filename = await this.storageProvider.saveFile(avatarFilename)
+		user.avatar = filename
 
 		await this.usersRepository.save(user)
 
