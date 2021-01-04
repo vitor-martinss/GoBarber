98b764150c9596647ea01879c3fa5a5e90c2cc57
Vitor Martins <vitormartinsweb@gmail.com>
2021-01-04 09:10:28 +0000

    UPD reset password

--- gobarber-api/src/modules/users/services/CreateUserService.spec.ts
+++ gobarber-api/src/modules/users/services/CreateUserService.spec.ts
@@ -2,13 +2,18 @@ import AppError from '@shared/errors/AppError'
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
 import CreateUserService from './CreateUserService'
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider'
+let fakeUsersRepository: FakeUsersRepository
+let fakeHashProvider: FakeHashProvider
+let createUser: CreateUserService
 
 describe('CreateUser', () => {
-	it('should be able to create a new user', async () => {
-		const fakeUsersRepository = new FakeUsersRepository()
-		const fakeHashProvider = new FakeHashProvider()
-		const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider)
+	beforeEach(() => {
+		fakeUsersRepository = new FakeUsersRepository()
+		fakeHashProvider = new FakeHashProvider()
+		createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider)
+	})
 
+	it('should be able to create a new user', async () => {
 		const user = await createUser.execute({
 			name: 'John Doe',
 			email: 'johndoe@example.com',
@@ -17,12 +22,7 @@ describe('CreateUser', () => {
 		expect(user).toHaveProperty('id')
 	})
 
-
 	it('should not be able to create a new user with same email from another', async () => {
-		const fakeUsersRepository = new FakeUsersRepository()
-		const fakeHashProvider = new FakeHashProvider()
-		const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider)
-
 		await createUser.execute({
 			name: 'John Doe',
 			email: 'johndoe@example.com',
