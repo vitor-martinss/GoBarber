a713f3887094d838d8ae607b9ec9526983927cb6
Vitor Martins <vitormartinsweb@gmail.com>
2021-01-07 17:55:48 +0000

    ADD list appointments

--- gobarber-api/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ gobarber-api/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -4,7 +4,7 @@ import ListProviderDayAvailabilityService from '@modules/appointments/services/L
 
 export default class ProviderDayAvailabilityController {
 	public async index(request: Request, response: Response): Promise<Response> {
-		const { provider_id }= request.params
+		const { provider_id } = request.params
 		const { day, month, year } = request.body
 		const listProviderDayAvailabilityService = container.resolve(ListProviderDayAvailabilityService)
 
--- gobarber-api/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ gobarber-api/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 import AppointmentController from '../controllers/AppointmentsController'
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController'
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 
 const appointmentsRouter = Router()
 const appointmentsController = new AppointmentController()
+const providerAppointmentsController = new ProviderAppointmentsController()
 
 appointmentsRouter.use(ensureAuthenticated)
 
 appointmentsRouter.post('/', appointmentsController.create)
+appointmentsRouter.post('/me', providerAppointmentsController.index)
 
 export default appointmentsRouter
