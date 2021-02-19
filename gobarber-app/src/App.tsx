dc67a618867b00129d39b22c2cb13149e260aa35
Vitor Martins <vitormartinsweb@gmail.com>
2021-02-19 12:59:06 +0000

    ADD splashScreen for app

--- gobarber-app/src/App.tsx
+++ gobarber-app/src/App.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'react-native-gesture-handler'
 import {View, StatusBar} from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
 import AppProvider from './hooks'
 import Routes from './routes'
-const App: React.FC = () => (
-	<NavigationContainer>
-		<StatusBar barStyle='light-content' backgroundColor='#312e38' translucent/>
-		<AppProvider>
-			<View style={{ flex:1, backgroundColor: '#312e38'}}>
-				<Routes />
-			</View>
-		</AppProvider>
+import SplashScreen from 'react-native-splash-screen'
 
-	</NavigationContainer>
-)
+const App: React.FC = () => {
+	useEffect(() => {
+		SplashScreen.hide()
+	},[])
+
+	return (
+		<NavigationContainer>
+			<StatusBar barStyle='light-content' backgroundColor='#312e38' translucent/>
+			<AppProvider>
+				<View style={{ flex:1, backgroundColor: '#312e38'}}>
+					<Routes />
+				</View>
+			</AppProvider>
+
+		</NavigationContainer>
+	)
+}
+
+
 
 export default App
